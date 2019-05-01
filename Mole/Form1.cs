74c9e11e798d6b2c59383fe37fffdc53f0fc68e4
박준용 <jypark.t1@gmail.com>
2019-05-02 03:10:12 +0900

    프로그램 수정

---
+++ Mole/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mole
+{
+    public partial class Form1 : Form
+    {
+        Mole mole;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                mole = new Mole(random, new Mole.PopUp(MoleCallBack));
+                timer1.Interval = random.Next(500, 1000);
+                timer1.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Application.Exit();
+
+            }
+            
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            ToggleMole();
+        }
+        private void ToggleMole()
+        {
+            if(mole.Hidden == true)
+            {
+                mole.Show();
+            }
+            else
+            {
+                mole.HideAgain();
+            }
+            timer1.Interval = random.Next(500, 1000);
+            timer1.Start();
+        }
+        private void MoleCallBack(int MoleNumber, bool Show)
+        {
+            if(MoleNumber < 0)
+            {
+                timer1.Stop();
+                return;
+            }
+            Button button;
+            switch (MoleNumber)
+            {
+                case 0: button = button1; break;
+                case 1: button = button2; break;
+                case 2: button = button3; break;
+                case 3: button = button4; break;
+                default:button = button5; break;
+            }
+            if(Show == true)
+            {
+                button.Text = "Hit me!";
+                button.BackColor = Color.Red;
+            }
+            else
+            {
+                button.Text = "";
+                button.BackColor = SystemColors.Control;
+            }
+            timer1.Interval = random.Next(500, 1000);
+            timer1.Start();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            mole.Smacked(0);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            mole.Smacked(1);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            mole.Smacked(2);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            mole.Smacked(3);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            mole.Smacked(4);
+        }
+    }
+}
---
+++ Mole/Mole.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mole
+{
+    class Mole
+    {
+        public delegate void PopUp(int hole, bool show);
+        private PopUp popUpCallback;
+        private bool hidden;
+        public bool Hidden { get { return hidden; } }
+        private int timesHit = 0;
+        private int timesShown = 0;
+        private int hole = 0;
+        Random random;
+
+        public Mole(Random random, PopUp popUpcallback)
+        {
+            if (popUpcallback == null)
+                throw new ArgumentException("PopupCallback can't be null");
+            this.random = random;
+            this.popUpCallback = popUpcallback;
+            hidden = true;
+        }
+        public void Show()
+        {
+            timesShown++;
+            hidden = false;
+            hole = random.Next(5);
+            popUpCallback(hole, true);//두더지가 자기자신을 보여준 후에 버튼의 색을 붉은색으로, 텍스트를 "Hit me"로 변경해서 두더지를 보여주는 폼에 있는 메소드를 호출해야 함
+        }
+        public void HideAgain()
+        {
+            hidden = true;
+            popUpCallback(hole, false);
+            CheckForGameOver();
+        }
+        public void Smacked(int holeSmacked)
+        {
+            if(holeSmacked == hole)
+            {
+                timesHit++;
+                hidden = true;
+                CheckForGameOver();
+                popUpCallback(hole, false);
+            }
+        }
+        private void CheckForGameOver()
+        {
+            if(timesShown >= 10)
+            {
+                popUpCallback(-1, false);
+                MessageBox.Show("You scored" + timesHit, "Game over");
+                Application.Exit();
+            }
+        }
+    }
+}
